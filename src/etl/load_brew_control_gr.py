301d9276037c51c3441b72350de6b650141f8bbf
bacross <bacross@hotmail.com>
2018-10-08 15:47:12 -0400

    fixed again brewcontrolgroup

--- src/etl/load_brew_control_gr.py
+++ src/etl/load_brew_control_gr.py
@@ -4,10 +4,6 @@ import csv
 
 def load_brew_control_group():
     brewContDf = pd.read_csv(cfg.brew_control_group_path, encoding = "iso-8859-1")
-    brewContDf = brewContDf.iloc[:, 0:4]
-    brewContDf.columns = brewContGrList[0]
-    brewContDf.index = brewContDf.iloc[:, 0]
-    
     controlGrBBL = pd.DataFrame(brewContDf['2016 Barrels (Control Group)'])
 
     return controlGrBBL
