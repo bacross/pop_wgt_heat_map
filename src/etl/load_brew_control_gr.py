1df5771f422a324c8e3dc1c155fd857d3f2cd10b
bacross <bacross@hotmail.com>
2018-10-08 15:45:26 -0400

    fixed brewcontrolgroup

--- src/etl/load_brew_control_gr.py
+++ src/etl/load_brew_control_gr.py
@@ -1,19 +1,13 @@
 import cfg
 import pandas as pd
+import csv
 
 def load_brew_control_group():
-    with open(cfg.brew_control_group_path) as brew_control_group_file:
-        brew_control_group = csv.reader(brew_control_group_file)
-        brew_control_group_list = [','.join(row) for row in brew_control_group]
-
-    brewContGrList = [strg.split(',') for strg in brew_control_group_list]
-
-    brewContDf = pd.DataFrame(brewContGrList[1:len(brewContGrList)])
+    brewContDf = pd.read_csv(cfg.brew_control_group_path, encoding = "iso-8859-1")
     brewContDf = brewContDf.iloc[:, 0:4]
     brewContDf.columns = brewContGrList[0]
     brewContDf.index = brewContDf.iloc[:, 0]
-    brewContDf.head()
-
+    
     controlGrBBL = pd.DataFrame(brewContDf['2016 Barrels (Control Group)'])
 
     return controlGrBBL
